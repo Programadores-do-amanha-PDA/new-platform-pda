17e7a3d25e699873cf9627354b27881a07fa2c13
Karlla Souzza <karlla.souzza7@gmail.com>
2025-01-23 00:36:46 -0300

    fix(admin): all jobs page paddings and gaps

--- app/dashboard/admin/jobs/all/page.tsx
+++ app/dashboard/admin/jobs/all/page.tsx
@@ -7,22 +7,26 @@ import { ChevronRight } from "lucide-react";
 
 export default function Home() {
   return (
-    <main className="relative w-full h-full p-6 lg:gap-10 lg:p-8">
-      <div className="w-full min-w-0">
-        <div className="mb-4 flex h-5 items-center space-x-1 text-sm leading-none">
-          <SidebarTrigger />
-          <Separator orientation="vertical" className="!mx-3" />
-          <div className="truncate text-muted-foreground">Inicio</div>
-          <ChevronRight className="h-3.5 w-3.5" />
-          <div className="text-muted-foreground">Vagas</div>
-          <ChevronRight className="h-3.5 w-3.5" />
-          <div className="text-foreground">Todas as vagas</div>
+    <main className="relative w-full flex flex-col p-6 gap-4 xl:p-8">
+      <div className="mb-4 flex h-5 items-center space-x-1 text-sm leading-none">
+        <SidebarTrigger />
+        <Separator orientation="vertical" className="!mx-3" />
+        <div className="truncate text-muted-foreground">Inicio</div>
+        <ChevronRight className="h-3.5 w-3.5" />
+        <div className="text-muted-foreground">Vagas</div>
+        <ChevronRight className="h-3.5 w-3.5" />
+        <div className="text-foreground">Todas as vagas</div>
+      </div>
+
+      <div className="flex flex-col gap-4">
+        <div className="flex justify-between items-center">
+          <div className="space-y-2 flex flex-col gap-1">
+            <h1 className={cn("scroll-m-20 text-3xl font-bold tracking-tight")}>
+              Todas as vagas
+            </h1>
+          </div>
         </div>
-        <div className="space-y-2">
-          <h1 className={cn("scroll-m-20 text-3xl font-bold tracking-tight")}>
-            Todos as vagas
-          </h1>
-        </div>
+
         <div className="space-y-2">
           <JobsDataTable />
         </div>
