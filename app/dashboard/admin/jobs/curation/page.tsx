c951b7878859ea5750c65c6994416c38ab686bd9
Karlla Souzza <karlla.souzza7@gmail.com>
2025-01-24 21:14:30 +0000

    feat: adding a read curation jobs

---
+++ app/dashboard/admin/jobs/curation/page.tsx
@@ -0,0 +1,38 @@
+"use client";
+import { cn } from "@/lib/utils";
+
+import { Separator } from "@/components/ui/separator";
+import { SidebarTrigger } from "@/components/ui/sidebar";
+import JobsDataTable from "@/components/jobs/curation/jobs-data-table";
+
+import { ChevronRight } from "lucide-react";
+
+export default function Home() {
+  return (
+    <main className="relative w-full flex flex-col p-6 gap-4 xl:p-8">
+      <div className="mb-4 flex h-5 items-center space-x-1 text-sm leading-none">
+        <SidebarTrigger />
+        <Separator orientation="vertical" className="!mx-3" />
+        <div className="truncate text-muted-foreground">Inicio</div>
+        <ChevronRight className="h-3.5 w-3.5" />
+        <div className="text-muted-foreground">Vagas</div>
+        <ChevronRight className="h-3.5 w-3.5" />
+        <div className="text-foreground">Curadoria de vagas</div>
+      </div>
+
+      <div className="flex flex-col gap-4">
+        <div className="flex justify-between items-center">
+          <div className="space-y-2 flex flex-col gap-1">
+            <h1 className={cn("scroll-m-20 text-3xl font-bold tracking-tight")}>
+              Curadoria de vagas
+            </h1>
+          </div>
+        </div>
+
+        <div className="space-y-2">
+          <JobsDataTable />
+        </div>
+      </div>
+    </main>
+  );
+}
---
+++ components/jobs/curation/jobs-data-table.tsx
@@ -0,0 +1,264 @@
+"use client";
+
+import { ColumnDef } from "@tanstack/react-table";
+import { DataTable } from "../data-table";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Button } from "@/components/ui/button";
+import { ArrowUpDown, MoreHorizontal } from "lucide-react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { useEffect, useState } from "react";
+
+import axios from "axios";
+import { Badge } from "@/components/ui/badge";
+import { JobDetailsType, JobType } from "@/types/jobs";
+import { toast } from "sonner";
+import JobSheetData from "../job-sheet-data";
+
+const JobsDataTable = () => {
+  const [jobs, setJobs] = useState<JobType[]>([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchJobs = async () => {
+      setLoading(true);
+      try {
+        const response = await axios.get("/api/jobs");
+        setJobs(response.data.results);
+      } catch (error) {
+        console.error(error);
+      }
+
+      setLoading(false);
+    };
+
+    fetchJobs();
+  }, []);
+
+  const handleDeleteJob = async (jobId: string) => {
+    try {
+      const response = await axios.delete(`/api/jobs?id=${jobId}`);
+      if (response.status === 200) {
+        toast.success("Vaga deletada com sucesso!");
+        const filteredProfiles = jobs.filter((job) => job.id !== jobId);
+        setJobs(filteredProfiles);
+        return true;
+      }
+    } catch (error) {
+      console.error("Error deleting job:", error);
+      toast.error("Erro ao deletar a vaga");
+      return false;
+    }
+  };
+
+  const handleCurateJob = async (jobId: string) => {
+    try {
+      const data = {
+        jobId: jobId,
+        updates: {
+          curated: true,
+        },
+      };
+
+      const response = await axios.put(`/api/jobs`, data);
+      if (response.status === 200) {
+        const filteredProfiles = jobs.map((job) =>
+          job.id === jobId ? { ...job, curated: true } : job
+        );
+        setJobs(filteredProfiles);
+
+        toast.success("Vaga aprovada com sucesso!");
+        return true;
+      }
+    } catch (error) {
+      console.error("Error to curate job:", error);
+      toast.error("Erro ao aprovar a vaga.");
+      return false;
+    }
+  };
+
+  const handleSetNewJob = (newJob: JobType) => {
+    setJobs((jobs) => [...jobs, newJob]);
+  };
+
+  const handleUpdateJob = (newJob: JobType) => {
+    const updatedJobs = jobs.map((job) =>
+      job.id === newJob.id ? newJob : job
+    );
+    setJobs(updatedJobs);
+  };
+
+  const columns: ColumnDef<JobType>[] = [
+    {
+      id: "select",
+      header: ({ table }) => (
+        <Checkbox
+          checked={
+            table.getIsAllPageRowsSelected() ||
+            (table.getIsSomePageRowsSelected() && "indeterminate")
+          }
+          onCheckedChange={(value: unknown) =>
+            table.toggleAllPageRowsSelected(!!value)
+          }
+          aria-label="Select all"
+          className="mx-2"
+        />
+      ),
+      cell: ({ row }) => (
+        <Checkbox
+          checked={row.getIsSelected()}
+          onCheckedChange={(value: unknown) => row.toggleSelected(!!value)}
+          aria-label="Select row"
+        />
+      ),
+      enableSorting: false,
+      enableHiding: false,
+    },
+    {
+      accessorKey: "title",
+      header: ({ column }) => {
+        return (
+          <Button
+            variant="ghost"
+            className="text-left px-2"
+            onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+          >
+            Titulo
+            <ArrowUpDown />
+          </Button>
+        );
+      },
+      cell: ({ row }) => (
+        <div className="lowercase">{row.getValue("title")}</div>
+      ),
+    },
+    {
+      accessorKey: "company",
+      header: ({ column }) => {
+        return (
+          <Button
+            variant="ghost"
+            className="px-2"
+            onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+          >
+            Empresa
+            <ArrowUpDown />
+          </Button>
+        );
+      },
+      cell: ({ row }) => (
+        <div className="lowercase">{row.getValue("company")}</div>
+      ),
+    },
+    {
+      accessorKey: "details",
+      header: ({ column }) => {
+        return (
+          <Button
+            variant="ghost"
+            className="px-2"
+            onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+          >
+            Localização
+            <ArrowUpDown />
+          </Button>
+        );
+      },
+      cell: ({ row }) => (
+        <div className="lowercase">
+          {row.getValue<JobDetailsType>("details").locale[0]}
+        </div>
+      ),
+    },
+    {
+      accessorKey: "languages",
+      header: () => <div className="px-2 text-left">Tecnologias</div>,
+
+      cell: ({ row }) => {
+        return row
+          .getValue<JobDetailsType>("details")
+          .languages?.map((language, i) => (
+            <Badge variant="outline" key={i}>
+              {language}
+            </Badge>
+          ));
+      },
+    },
+    {
+      accessorKey: "workplace_type",
+      header: () => <div className="px-2 text-left">Local de Trabalho</div>,
+
+      cell: ({ row }) => {
+        return row
+          .getValue<JobDetailsType>("details")
+          .workplace_type?.map((w, i) => (
+            <Badge variant="outline" key={i}>
+              {w}
+            </Badge>
+          ));
+      },
+    },
+    {
+      id: "actions",
+      enableHiding: false,
+      cell: ({ row }) => {
+        const job = row.original;
+
+        return (
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="ghost" className="h-8 w-8 p-0">
+                <span className="sr-only">Abrir Menu</span>
+                <MoreHorizontal />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent
+              align="end"
+              className="max-w-52 flex flex-col gap-1"
+            >
+              <div>
+                <DropdownMenuLabel>Ações</DropdownMenuLabel>
+                <DropdownMenuSeparator />
+              </div>
+              <Button
+                onClick={() => handleCurateJob(job.id)}
+                variant="ghost"
+                className="!px-2 w-full h-max items-start justify-start text-start"
+              >
+                Aprovar Vaga
+              </Button>
+              <JobSheetData
+                mode="edit"
+                currentJob={job}
+                handleUpdateJobs={handleUpdateJob}
+              />
+              <Button
+                onClick={() => handleDeleteJob(job.id)}
+                variant="ghost"
+                className="!px-2 w-full h-max items-start justify-start text-start"
+              >
+                Deletar Vaga
+              </Button>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        );
+      },
+    },
+  ];
+
+  return (
+    <DataTable
+      columns={columns}
+      data={jobs.filter((job) => job.curated === false)}
+      loading={loading}
+      handleSetNewJob={handleSetNewJob}
+    />
+  );
+};
+
+export default JobsDataTable;
