c685d433742c712fd261712c73349e148d2634ba
Karlla Souzza <karlla.souzza7@gmail.com>
2025-09-01 13:39:27 -0300

    fix: adjust empty assessments text and description
    - adjust the useParams to use a type
    - adjust ui of the assessment list container
    - adding a empty assessment container and adjust the title and description

--- src/features/dashboard/classroom-coodesh/pages/assessments-page.tsx
+++ src/features/dashboard/classroom-coodesh/pages/assessments-page.tsx
@@ -9,13 +9,8 @@ import AssessmentsSheetData from "../components/assessments-sheet-data";
 import { useCoodeshAssessmentStore } from "../stores/assessments";
 
 export default function AssessmentsPage() {
-  const { classroom_id } = useParams();
+  const { classroom_id } = useParams<{ classroom_id: string }>();
   const [searchFilter, setSearchFilter] = useState<string>("");
-
-  // Ensure classroom_id is a string
-  const classroomId = Array.isArray(classroom_id)
-    ? classroom_id[0]
-    : classroom_id;
 
   const { assessments } = useCoodeshAssessmentStore();
 
@@ -24,7 +19,7 @@ export default function AssessmentsPage() {
   );
 
   return (
-    <div className="relative w-full h-max p-4 flex flex-col gap-6 overflow-hidden">
+    <div className="relative w-full h-full p-4 flex flex-col gap-6 overflow-hidden">
       <header className="w-full flex items-center justify-between flex-wrap p-2 gap-4">
         <Input
           id="search"
@@ -35,35 +30,35 @@ export default function AssessmentsPage() {
           onChange={(e) => setSearchFilter(e.target.value)}
         />
 
-        <AssessmentsSheetData classroom_id={classroomId} />
+        <AssessmentsSheetData classroom_id={classroom_id} />
       </header>
 
-      <ul className="p-2 h-full w-full flex flex-col sm:flex-row sm:flex-wrap gap-4 overflow-y-auto pr-4">
-        {filteredAssessments
-          ?.sort(
-            (a, b) =>
-              new Date(a.created_at).getTime() -
-              new Date(b.created_at).getTime()
-          )
-          .map((assessment) => (
-            <AssessmentsClassroomListCard
-              key={assessment.assessment_id}
-              assessment={assessment}
-              expansive={true}
-            />
-          ))}
-
-        {assessments?.length === 0 && (
-          <div className="flex flex-col gap-2 h-full w-full bg-red-100 items-center justify-center">
-            <h2 className="text-sm font-bold text-gray-800">
-              Não ha avaliações anexadas para essa turma.
-            </h2>
-            <i className="text-xs text-muted-foreground px-2 text-center">
-              (Assim que você anexar avaliações, elas aparecerão aqui.)
-            </i>
-          </div>
-        )}
-      </ul>
+      {assessments && assessments.length > 0 ? (
+        <ul className="p-2 h-max w-full flex flex-col sm:flex-row sm:flex-wrap gap-4 overflow-y-auto pr-4">
+          {filteredAssessments
+            ?.sort(
+              (a, b) =>
+                new Date(a.created_at).getTime() -
+                new Date(b.created_at).getTime()
+            )
+            .map((assessment) => (
+              <AssessmentsClassroomListCard
+                key={assessment.assessment_id}
+                assessment={assessment}
+                expansive={true}
+              />
+            ))}
+        </ul>
+      ) : (
+        <div className="w-full h-full flex flex-col gap-2 bg-primary/5 rounded-lg items-center justify-center">
+          <h2 className="font-bold text-xl text-foreground">
+            Não ha avaliações anexadas para essa turma.
+          </h2>
+          <i className="text-sm text-muted-foreground px-2 text-center">
+            (Assim que você anexar avaliações, elas aparecerão aqui.)
+          </i>
+        </div>
+      )}
     </div>
   );
 }
