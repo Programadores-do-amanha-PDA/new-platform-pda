fdd5234baf044069152ffb0139de4378488962e6
Karlla Souzza <karlla.souzza7@gmail.com>
2025-09-02 14:08:34 -0300

    feat: adding the sidebar item without collapsible

--- src/components/shared/sidebar/nav-main.tsx
+++ src/components/shared/sidebar/nav-main.tsx
@@ -34,51 +34,73 @@ export default function NavMain({
     <SidebarGroup>
       <SidebarGroupLabel>{title}</SidebarGroupLabel>
       <SidebarMenu>
-        {items.map((item) => (
-          <Collapsible
-            key={item.title}
-            asChild
-            defaultOpen={
-              path.split("/").includes(item?.ref || "") ? true : false
-            }
-            className="group/collapsible "
-          >
-            <SidebarMenuItem>
-              <CollapsibleTrigger asChild>
-                <SidebarMenuButton tooltip={item.title}>
-                  {item.icon && (
-                    <div onClick={() => router.push(item.url)}>
-                      <DynamicLucideIcon
-                        name={item.icon}
-                        className="size-4 cursor-pointer"
-                      />
-                    </div>
-                  )}
-                  <p
-                    onClick={() => router.push(item.url)}
-                    className="font-semibold cursor-pointer hover:underline"
-                  >
-                    {item.title}
-                  </p>
-                  <ChevronRight className="ml-auto transition-transform duration-200 group-data-[state=open]/collapsible:rotate-90" />
-                </SidebarMenuButton>
-              </CollapsibleTrigger>
-              <CollapsibleContent>
-                <SidebarMenuSub>
-                  {item.items?.map((subItem) => (
-                    <SidebarMenuSubButton
-                      key={subItem.title}
-                      onClick={() => router.push(subItem.url)}
-                      className="cursor-pointer hover:underline"
+        {items.map((item, i) =>
+          item.items && item.items?.length > 0 ? (
+            <Collapsible
+              key={`sidebar-item-${i}`}
+              asChild
+              defaultOpen={
+                path.split("/").includes(item?.ref || "") ? true : false
+              }
+              className="group/collapsible"
+            >
+              <SidebarMenuItem>
+                <CollapsibleTrigger asChild>
+                  <SidebarMenuButton tooltip={item.title}>
+                    {item.icon && (
+                      <div onClick={() => router.push(item.url)}>
+                        <DynamicLucideIcon
+                          name={item.icon}
+                          className="size-4 cursor-pointer"
+                        />
+                      </div>
+                    )}
+                    <p
+                      onClick={() => router.push(item.url)}
+                      className="font-semibold cursor-pointer hover:underline"
                     >
-                      {subItem.title}
-                    </SidebarMenuSubButton>
-                  ))}
-                </SidebarMenuSub>
-              </CollapsibleContent>
+                      {item.title}
+                    </p>
+
+                    <ChevronRight className="ml-auto transition-transform duration-200 group-data-[state=open]/collapsible:rotate-90" />
+                  </SidebarMenuButton>
+                </CollapsibleTrigger>
+                <CollapsibleContent>
+                  <SidebarMenuSub>
+                    {item.items?.map((subItem) => (
+                      <SidebarMenuSubButton
+                        key={subItem.title}
+                        onClick={() => router.push(subItem.url)}
+                        className="cursor-pointer hover:underline"
+                      >
+                        {subItem.title}
+                      </SidebarMenuSubButton>
+                    ))}
+                  </SidebarMenuSub>
+                </CollapsibleContent>
+              </SidebarMenuItem>
+            </Collapsible>
+          ) : (
+            <SidebarMenuItem key={`sidebar-item-${i}`}>
+              <SidebarMenuButton
+                tooltip={item.title}
+                onClick={() => router.push(item.url)}
+              >
+                {item.icon && (
+                  <div>
+                    <DynamicLucideIcon
+                      name={item.icon}
+                      className="size-4 cursor-pointer"
+                    />
+                  </div>
+                )}
+                <p className="font-semibold cursor-pointer hover:underline">
+                  {item.title}
+                </p>
+              </SidebarMenuButton>
             </SidebarMenuItem>
-          </Collapsible>
-        ))}
+          )
+        )}
       </SidebarMenu>
     </SidebarGroup>
   );
