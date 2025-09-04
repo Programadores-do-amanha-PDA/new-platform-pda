6536ad4ab3f7354378b50450a9fdd2a2f689ec71
Karlla Souzza <karlla.souzza7@gmail.com>
2025-09-04 01:31:41 -0300

    feat: adding classroom modules on overview

--- src/features/dashboard/classroom-overview/components/classroom-overview-table.tsx
+++ src/features/dashboard/classroom-overview/components/classroom-overview-table.tsx
@@ -1,15 +1,32 @@
 "use client";
 
 import { ClassroomOverviewData } from "@/types/classroom-overview";
+import { ClassroomConfigModulesT } from "@/types/classroom-configs";
 import { DataTable } from "./data-table";
 import { createColumns } from "./columns";
 
 interface ClassroomOverviewTableProps {
   data: ClassroomOverviewData;
+  modules?: ClassroomConfigModulesT[];
+  onDateRangeChange?: (dateRange: { from: Date; to: Date }) => void;
 }
 
-export function ClassroomOverviewTable({ data }: ClassroomOverviewTableProps) {
+export function ClassroomOverviewTable({
+  data,
+  modules = [],
+  onDateRangeChange,
+}: ClassroomOverviewTableProps) {
   const columns = createColumns(data);
 
-  return <DataTable columns={columns} data={data.students} fullData={data} />;
+  return (
+    <div className="flex flex-col gap-4 h-full">
+      <DataTable
+        columns={columns}
+        data={data.students}
+        fullData={data}
+        onDateRangeChange={onDateRangeChange}
+        modules={modules}
+      />
+    </div>
+  );
 }
